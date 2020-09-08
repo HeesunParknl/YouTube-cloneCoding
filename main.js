7f2db4bb3a10443ed0ac33dcfe0cdf28b9dd6d75
Heesun <68651370+HeesunParknl@users.noreply.github.com>
2020-09-08 18:30:30 +0200

    added night mode

--- main.js
+++ main.js
@@ -5,3 +5,23 @@ moreBtn.addEventListener('click', () => {
     moreBtn.classList.toggle('clicked');
     title.classList.toggle('clamp');
 });
+
+
+
+
+function switchTheme() {
+    var checkbox = document.getElementById('checkbox');
+    if (checkbox.checked == true) {
+        document.querySelector('body').style.background = 'black';
+        document.documentElement.style.setProperty('--white', '#000000');
+        document.documentElement.style.setProperty('--black', '#ffffff');
+        document.documentElement.style.setProperty('--grey', 'lightgray');
+        document.documentElement.style.setProperty('--blue', 'powderblue');
+    } else {
+        document.querySelector('body').style.background = 'white';
+        document.documentElement.style.removeProperty('--white', '#000000');
+        document.documentElement.style.removeProperty('--black', '#ffffff');
+        document.documentElement.style.removeProperty('--grey', 'lightgray');
+        document.documentElement.style.removeProperty('--blue', 'powderblue');
+    }
+}
